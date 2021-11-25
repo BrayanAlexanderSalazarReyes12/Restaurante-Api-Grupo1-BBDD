ae681b3aa0c33130c02e3dc74fe4d6bfa5056a33
BrayanAlexanderSalazarReyes12 <brayanreyes2008@hotmail.com>
2021-11-25 15:56:52 -0500

    Agregar archivos de proyecto.

---
+++ Restaurante-Api-Grupo1-BBDD/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurante_Api_Grupo1_BBDD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDemo()
+        {
+            return Content("Hola desde la versión por defecto redesplegando en heroku");
+        }
+    }
+}
